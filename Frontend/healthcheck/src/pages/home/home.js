dffcb756c6471167e76542675d4c753b8f98c160
Minhhu <minh.nq40153@sinhvien.hoasen.edu.vn>
2024-06-22 04:34:04 +0700

    wew
    ww

--- Frontend/healthcheck/src/pages/home/home.js
+++ Frontend/healthcheck/src/pages/home/home.js
@@ -9,17 +9,17 @@ function Home() {
       <div class="home">
         <div class="home-box">
           <h1>You have been scanned!</h1>
-          <p>Health Check is a web application that helps you to monitor your health.</p>
-          <Radar />
-          {/* <div class='radar-container'>        
+          <p>By our state of the art Space radar array.</p>
+          <div class='radar-container'>        
             <div class='radar'>
-              <div class='sweep'></div>
+              <div class='sweep'>
+              </div>              
               <div class='ping-dot' id="dot1"></div>
               <div class='ping-dot' id="dot2"></div>
               <div class='ping-dot' id="dot3"></div>
             </div>
-          </div> */}
-          <p>Click on the links above to get started!</p>
+          </div>
+          <button class="gotologin-button">More detail</button>
         </div>
       </div>
     </div>
